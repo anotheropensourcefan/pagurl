dced4969a12997022d304e0abeb6ab1484bf40b2
anotheropensourcefan <94914448+anotheropensourcefan@users.noreply.github.com>
2025-04-11 20:32:26 +0200

    v1.2.0: vault-wide no-duplicates option
    - added vault-wide no-duplicates option: if enabled, there will never be 2 notes with the same url in the entire vault, otherwise it will only check the specified folder before creating a new file with an url
    - made it more versatile: if the selected text is not a link (on desktop only), it will work, except there won't be a "url" property
    - cleaned the code: removed unnecessary logging and command prefixes, reorganized the main.ts, fixed the duplicate search
    - simplified naming: a "note" is a representation of a file (name + content) ; the "file" is what's on disk

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Sometimes you want to reference a webpage, and sometimes you want to write more 
 1. Put your cursor anywhere inside a link: [hello world](https://hello-world.com)
 2. Command Palette -> Pagurl
    Pick a folder (defined in settings)
-3. A new note has been created, with "url" and "aliases" properties
+3. A new note has been created (or linked to if there is already a note with this url), with "url" and "aliases" properties
    Modify the new note as you wish (including its title)
    Keep intact the "aliases" property
 
--- main.ts
+++ main.ts
@@ -1,203 +1,217 @@
 // this Obsidian plugin turns a URL link into a page
-import { Plugin, Editor, moment, Notice, PluginSettingTab, App, Setting } from "obsidian";
+import { Plugin, Editor, moment, Notice, PluginSettingTab, App, Setting } from "obsidian"
 
-interface URLToPageSettings {
-	template: string;
-	fileNameStrategy: "title" | "domain-date";
-	folders: string[];
+interface PagurlSettings {
+	template: string
+	vaultWideNoDuplicate: boolean
+	nameStrategy: "title" | "domain-date"
+	folders: string[]
 }
 
-const DEFAULT_SETTINGS: Partial<URLToPageSettings> = {
+const DEFAULT_SETTINGS: Partial<PagurlSettings> = {
 	template: "",
-	fileNameStrategy: "title",
+	vaultWideNoDuplicate: false,
+	nameStrategy: "title",
 	folders: [],
-};
+}
 
 interface ExtractedLink {
-	fullMatch: string;
-	alias: string;
-	url: string;
+	fullMatch: string
+	alias: string
+	url: string
 }
 
 
 export default class PagurlPlugin extends Plugin {
-	settings: URLToPageSettings;
+	settings: PagurlSettings
 
 	async onload() {
-		await this.loadSettings();
+		await this.loadSettings()
 
-		this.addCommands(this.settings.folders);
+		this.addCommands(this.settings.folders)
 
 		this.addCommand({
-			id: 'pagurl-current',
+			id: 'current',
 			name: 'current',
 			editorCallback: async (editor: Editor) => {
-				await this.callback(editor, 'current');
+				await this.callback(editor, 'current')
 			}
-		});
+		})
 
-		this.addSettingTab(new SettingTab(this.app, this));
+		this.addSettingTab(new SettingTab(this.app, this))
 	}
+
 	private command(name: string) {
 		return {
-			id: `pagurl-${name}`,
+			id: name,
 			name,
 			editorCallback: async (editor: Editor) => {
-				await this.callback(editor, name);
+				await this.callback(editor, name)
 			}
 		}
 	}
-	private async callback(editor: Editor, folder: string) {
-		try {
-			if (folder === 'current') {
-				const activeFile = this.app.workspace.getActiveFile();
-				if (!activeFile) throw new Error("No active file to determine its folder.");
-				folder = activeFile?.parent?.path || folder;
-			}
 
-			let link = this.extractMarkdownLink(editor);
-			if (!link) {
-				const selectedText = editor.getSelection().trim();
-				if (!selectedText) {
-					throw new Error("Select a valid text or Markdown link");
-				}
-				link = {
-					fullMatch: selectedText,
-					alias: selectedText,
-					url: ""
-				};
-			}
-
-			const { newFileName, content } = await this.processLink(link);
-			await this.createNote(newFileName, content, folder);
-			this.replaceMarkdownLink(editor, link.fullMatch, newFileName);
-
-			new Notice(`Créé: ${newFileName}`);
-		} catch (error) {
-			new Notice(`Attention! ${error.message}`);
-		}
-	}
 	private addCommands(names: string[]) {
 		for (const name of names) {
-			this.addCommand(this.command(name));
+			this.addCommand(this.command(name))
 		}
 	}
-	private extractMarkdownLink(editor: Editor): ExtractedLink | null {
-		const selection = editor.getSelection();
-		const cursorPos = editor.getCursor();
-		const lineContent = editor.getLine(cursorPos.line);
 
-		return this.findMarkdownLink(selection || lineContent);
+	private async callback(editor: Editor, folder: string) {
+		if (folder === 'current') {
+			const activeFile = this.app.workspace.getActiveFile()
+			if (!activeFile) throw new Error("No active file to determine its folder.")
+			folder = activeFile?.parent?.path || folder
+		}
+
+		let link = this.extractMarkdownLink(editor)
+		if (!link) {
+			const selectedText = editor.getSelection().trim()
+			if (!selectedText) {
+				throw new Error("Select a valid text or Markdown link")
+			}
+			link = {
+				fullMatch: selectedText,
+				alias: selectedText,
+				url: ""
+			}
+		}
+		try {
+
+			const existingFile = this.findExistingFile(link.url, folder)
+			let name = existingFile?.name
+
+			if (!existingFile) {
+				const note = await this.generateNote(link)
+				name = note.name
+				await this.createFile(name, note.content, folder)
+			}
+			if (name) {
+				this.replaceMarkdownLink(editor, link.fullMatch, name, link.alias)
+				new Notice(`Pagurl: ${link.url} ${existingFile ? '-->' : '==>'} ${existingFile?.folder || folder}/${name}`)
+				// --> a link to an existing file
+				// ==> a newly created file
+			}
+		} catch (error) {
+			new Notice(`Pagurl error: ${error.message}`)
+		}
+	}
+
+	private extractMarkdownLink(editor: Editor): ExtractedLink | null {
+		const selection = editor.getSelection()
+		const cursorPos = editor.getCursor()
+		const lineContent = editor.getLine(cursorPos.line)
+
+		return this.findMarkdownLink(selection || lineContent)
 	}
 
 	private findMarkdownLink(text: string): ExtractedLink | null {
-		const mdLinkRegex = /\[([^\]]+)\]\(\s*<?(https?:\/\/[^\s>]+)>?\s*\)/i;
-		const match = text.match(mdLinkRegex);
-		if (!match) return null;
+		const mdLinkRegex = /\[([^\]]+)\]\(\s*<?(https?:\/\/[^\s>]+)>?\s*\)/i
+		const match = text.match(mdLinkRegex)
+		if (!match) return null
 
 		return {
 			fullMatch: match[0],
 			alias: match[1].trim(),
 			url: match[2].trim()
-		};
-	}
-
-	private async processLink(link: ExtractedLink): Promise<{
-		newFileName: string;
-		content: string;
-	}> {
-		const existingNote = this.findExistingNote(link.url);
-		if (existingNote) {
-			return {
-				newFileName: existingNote,
-				content: ""
-			};
 		}
+	}
 
+	private async generateNote(link: ExtractedLink): Promise<{
+		name: string
+		content: string
+	}> {
 		return {
-			newFileName: this.generateFileName(link),
-			content: this.generateNoteContent(link)
-		};
+			name: this.generateName(link),
+			content: this.generateContent(link)
+		}
 	}
 
-	private findExistingNote(url: string): string | null {
-		return this.app.vault.getFiles().find(file =>
-			file.frontmatter?.url === url
-		)?.basename || null;
+	private findExistingFile(url: string, folder: string) {
+
+		const file = this.app.vault.getMarkdownFiles()
+			.filter(file => {
+				return this.settings.vaultWideNoDuplicate || file.parent?.path === folder
+			})
+			.find(file => {
+				const frontmatter = this.app.metadataCache.getFileCache(file)?.frontmatter
+				return frontmatter && frontmatter?.url === url
+			})
+		if (file) {
+			return { name: file.basename, folder: file.parent?.path }
+		}
+		return null
 	}
 
-	private generateFileName(link: ExtractedLink): string {
-		const sanitizedAlias = this.sanitizeFileName(link.alias);
-		if (!link.url) return sanitizedAlias;
-		switch (this.settings.fileNameStrategy) {
+	private generateName(link: ExtractedLink): string {
+		const sanitizedAlias = this.sanitizeName(link.alias)
+		if (!link.url) return sanitizedAlias
+		switch (this.settings.nameStrategy) {
 			case "domain-date":
 				try {
-					const domain = new URL(link.url).hostname;
-					const date = moment().format("YYYY-MM-DD");
-					return `${domain}-${date}`;
+					const domain = new URL(link.url).hostname
+					const date = moment().format("YYYY-MM-DD")
+					return `${domain}-${date}`
 				} catch {
-					return sanitizedAlias;
+					return sanitizedAlias
 				}
 			default:
-				return sanitizedAlias;
+				return sanitizedAlias
 		}
 	}
 
-	private sanitizeFileName(name: string): string {
-		return name.replace(/[\/\\#%&\{\}\|<>*?$!'":@]/g, "-");
+	private sanitizeName(name: string): string {
+		return name.replace(/[\/\\#%&\{\}\|<>*?$!'":@]/g, "-")
 	}
 
-	private generateNoteContent(link: ExtractedLink): string {
-		const frontmatter = `---\n` + (link.url ? `url: "${link.url}"\n` : ``) + `aliases:\n  - ${link.alias}\n---\n`;
-		// const frontmatter = `---\nurl: "${link.url}"\naliases:\n  - ${link.alias}\n---\n`;
+	private generateContent(link: ExtractedLink): string {
+		const frontmatter = `---\n` + (link.url ? `url: "${link.url}"\n` : ``) + `aliases:\n  - ${link.alias}\n---\n`
 		const content = this.settings.template
 			.replace(/{{alias}}/g, link.alias)
-			.replace(/{{url}}/g, link.url);
+			.replace(/{{url}}/g, link.url)
 
-		return frontmatter + content;
+		return frontmatter + content
 	}
 
-	private async createNote(fileName: string, content: string, folder: string): Promise<void> {
-		await this.app.vault.create(`${folder}/${fileName}.md`, content);
+	private async createFile(name: string, content: string, folder: string): Promise<void> {
+		await this.app.vault.create(`${folder}/${name}.md`, content)
 	}
 
-	private replaceMarkdownLink(editor: Editor, originalMatch: string, newFileName: string): void {
-		const cursor = editor.getCursor();
-		const content = editor.getValue();
-		const newLink = `[[${newFileName}|${newFileName}]]`;
+	private replaceMarkdownLink(editor: Editor, originalMatch: string, name: string, alias?: string): void {
+		const cursor = editor.getCursor()
+		const content = editor.getValue()
+		const newLink = `[[${name}|${alias || name}]]`
 
 		const newContent = content.replace(
 			originalMatch,
 			newLink
-		);
+		)
 
-		editor.setValue(newContent);
-		editor.setCursor(cursor);
+		editor.setValue(newContent)
+		editor.setCursor(cursor)
 	}
 
 	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData())
 	}
 
 	async saveSettings() {
-		await this.saveData(this.settings);
+		await this.saveData(this.settings)
 	}
 }
 
 class SettingTab extends PluginSettingTab {
-	plugin: PagurlPlugin;
+	plugin: PagurlPlugin
 
 	constructor(app: App, plugin: PagurlPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
+		super(app, plugin)
+		this.plugin = plugin
 	}
 
 	display(): void {
-		const { containerEl } = this;
+		const { containerEl } = this
 
-		containerEl.empty();
-
-		containerEl.createEl('h2', { text: 'Settings for My Awesome Plugin.' });
+		containerEl.empty()
 
 		new Setting(containerEl)
 			.setName('Folders')
@@ -206,8 +220,17 @@ class SettingTab extends PluginSettingTab {
 				.setValue(this.plugin.settings.folders.join(',')) // Display the list as comma-separated
 				.onChange(async (value) => {
 					this.plugin.settings.folders = value.split(',').map(item => item.trim()); // Split and trim the input
-					await this.plugin.saveSettings();
-					console.log("List updated:", this.plugin.settings.folders);  // Log the updated list
-				}));
+					await this.plugin.saveSettings()
+				}))
+
+		new Setting(containerEl)
+			.setName('Vault-wide no-duplicate')
+			.setDesc('By default, Pagurl only checks if the current folder has a note with the same URL. If enabled, it will check the entire vault.')
+			.addToggle((toggle) => toggle
+				.setValue(this.plugin.settings.vaultWideNoDuplicate)
+				.onChange(async (value) => {
+					this.plugin.settings.vaultWideNoDuplicate = value
+					await this.plugin.saveSettings()
+				}))
 	}
 }
